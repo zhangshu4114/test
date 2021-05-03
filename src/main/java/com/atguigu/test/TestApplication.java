6a88ac6202e90be2ae2a73fa14d313c731e1a584
zhangshu4114 <zhangshu4114@163.com>
2021-05-03 16:33:34 +0800

    注册了一个bean

---
+++ src/main/java/com/atguigu/test/TestApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu.test;
+
+import org.apache.tomcat.jni.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+    @Bean
+    public User user(){
+        return new User();
+    }
+}
